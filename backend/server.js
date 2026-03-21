c60574a8530832ae2ba495d5258893f6db892ccd
MShabade <mpshabade@gmail.com>
2026-03-21 09:00:00 +0000

    feat: bootstrap Express server - cors, json parser, static middleware - followed a YouTube tutorial on Express.js middleware setup
    Express getting started: https://expressjs.com/en/starter/hello-world.html
    cors middleware npm: https://www.npmjs.com/package/cors
    express.static docs: https://expressjs.com/en/starter/static-files.html

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const resourceRoutes = require("./routes/resources");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend")));
+
